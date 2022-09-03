35202f6911c46f77333f84a3637629f5a220e6de
Natalia Shaurina <natashayrina@mail.ru>
2022-09-03 15:10:27 +0300

    задача 2

--- SeminarHW_002/Program.cs
+++ SeminarHW_002/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// int numA;
+// int numB;
+// int numC;
+
+Console.WriteLine("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if (numA > max) max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
