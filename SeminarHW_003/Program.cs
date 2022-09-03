9ba50c8e0fc46684be59dd2e7913923a994a6a8b
Natalia Shaurina <natashayrina@mail.ru>
2022-09-03 15:05:36 +0300

    задача3.

---
+++ SeminarHW_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// int num;
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (0 == num % 2 )
+{
+   Console.WriteLine("четное число" + num);
+}
+ if (1 == num % 2)
+{
+    Console.WriteLine("число нечетное" + num);
+} 
+ 
+  
