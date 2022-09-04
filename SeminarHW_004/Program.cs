bb2da6d8dce35aee18c036bb4e44464ad4c2bdf9
Natalia Shaurina <natashayrina@mail.ru>
2022-09-04 12:42:38 +0300

    задача 4.

---
+++ SeminarHW_004/Program.cs
@@ -0,0 +1,21 @@
+﻿// int num;
+
+Console.WriteLine("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= num)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i = i + 1;
+}
+
+
+
+
+
+
+
