0c169bfdb8a4dc66dfd525d3012e0f1cc961a7ef
Natalia Shaurina <natashayrina@mail.ru>
2022-09-04 17:18:42 +0300

    задача 10.

---
+++ Seminar2HW_10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вход трехзначное число, выход показывает вторую цифру этого числа. 456 - 5, 782 - 8, 918 - 1.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num / 100 * 100;
+int num2 = num % 10;
+int num3 = (num - num1 - num2) / 10;
+
+Console.WriteLine(num3);
+
+
+
