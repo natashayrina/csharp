a1d32d4e16ed96adb10e3dcbd42f8e498f9ec0d7
Natalia Shaurina <natashayrina@mail.ru>
2022-09-07 12:24:36 +0300

    задача 23.

---
+++ Seminar3HW_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа принимает число (n) и выдает таблицу кубов чисел от 1 до n.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= num)
+{
+  Console.WriteLine (Math.Pow (i, 3));
+  i++;  
+}
+
+
