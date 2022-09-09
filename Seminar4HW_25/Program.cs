83143d3e852d588537318b7ab4e82d26a40382ad
Natalia Shaurina <natashayrina@mail.ru>
2022-09-09 16:07:26 +0300

    задача 25.

---
+++ Seminar4HW_25/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите цикл, который принимает два числа А И В, и возводит А в натуральную степень В.
+
+Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+int res = 1;
+
+while (i <= B)
+{
+    res = (res * A);
+    Console.WriteLine(res);
+    i++;
+}
