7d583679188ac2e845a4c9c57309be472a73eae7
Natalia Shaurina <natashayrina@mail.ru>
2022-09-09 18:29:31 +0300

    задача 29.

---
+++ Seminar4HW_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    array[i] = new Random(). Next(0, 10);
+    Console.Write(array[i]);
+}
+
